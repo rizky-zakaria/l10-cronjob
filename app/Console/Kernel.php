ef02ad304b6ac3287a48a74177b846f967fd2375
Rizky Zakaria <rizky.zakaria78@gmail.com>
2024-02-15 20:12:15 +0700

    --allow-empty

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,7 +18,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('cronjob:run')->everyMinute();
+        // $schedule->command('cronjob:run')->everyMinute();
+        $schedule->command('cronjob:run')->cron('* * * * *');
     }
 
     /**
